a30cabff651802b6a5c23d0061f70c44b0d3ae3d
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2023-02-05 12:39:06 +0400

    fix

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,31 @@
 $(document).ready(function () {
     new Swiper('.reviews-swiper', {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 80,
-        autoplay:{delay: 2100}
+        autoplay: {delay: 2100},
+        breakpoints: {
+            820: {
+                slidesPerView: 2,
+            }
+        }
     })
     new Swiper('.rez-swiper', {
-        autoplay:{delay: 2000},
-        slidesPerView: 2,
-        spaceBetween: 80
+        autoplay: {delay: 2000},
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            820: {
+                slidesPerView: 2,
+                spaceBetween: 80
+            }
+        }
     })
+
+    $('a[href^="#"]').click(function () {
+        let anchor = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(anchor).offset().top
+        }, 0);
+    });
+
 })
